2c4b16c9e15773990f1e3f269bde02854ddd95d2
jesusemanuel87 <jesusemanuelgarcia@gmail.com>
2023-09-10 13:13:13 -0300

    TP04-2 Correccion Llamar

--- app/src/main/java/com/example/tp04_login_with_navigation/ui/llamar/LlamarViewModel.java
+++ app/src/main/java/com/example/tp04_login_with_navigation/ui/llamar/LlamarViewModel.java
@@ -33,13 +33,12 @@ public class LlamarViewModel extends AndroidViewModel {
             mensajeError.setValue("Por favor, introduce un número de teléfono válido");
         } else {
             Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("Teléfono:" + telefono));
-            //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono)));
+            intent.setData(Uri.parse("tel:" + telefono));
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             try {
                 context.startActivity(intent);
-            } catch (Exception e){
-                mensajeError.setValue("ERROR DE PERMISO");
+            } catch (Exception e) {
+                mensajeError.setValue("ERROR DE PERMISO o la función de llamada no está disponible en este dispositivo.");
             }
         }
     }
