f44ba537b7788f09f896f5aa767db005b236e4c9
Administrador <jesusemanuelgarcia@gmail.com>
2023-09-25 20:01:08 -0300

    Recup.2-correccion Salir de la app por completo

--- app/src/main/java/com/example/tp04_login_with_navigation/ui/salir/SalirDialogo.java
+++ app/src/main/java/com/example/tp04_login_with_navigation/ui/salir/SalirDialogo.java
@@ -3,6 +3,7 @@ package com.example.tp04_login_with_navigation.ui.salir;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 
 import com.example.tp04_login_with_navigation.MainActivity;
 
@@ -12,7 +13,9 @@ public class SalirDialogo {
         new AlertDialog.Builder(activity)
                 .setTitle("Salir")
                 .setMessage("¿Salir de la aplicación?")
-                .setPositiveButton("Si", (dialogInterface, i) -> System.exit(0))
+                .setPositiveButton("Si", (dialogInterface, i) -> {
+                    System.exit(0);
+                })
                 .setNegativeButton("No", (dialogInterface, i) -> {
                     Intent intent = new Intent(activity, MainActivity.class);
                     activity.startActivity(intent);
